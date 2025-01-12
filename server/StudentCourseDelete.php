206959a555347b2da5ac39d35e5e31aa62feb4a5
sidneyy0621 <C112156210@nkust.edu.tw>
2025-01-13 02:10:03 +0800

    註解掉的程式碼刪除

--- server/StudentCourseDelete.php
+++ server/StudentCourseDelete.php
@@ -9,15 +9,6 @@ function StudentCourseDelete(){
 
     if($response['status'] == 200) {
         $conn = $response['result'];
-
-        // Check if the student is enrolled in the course
-        // $check_sql = "SELECT * FROM `student_courses` WHERE student_name=? AND course_id=?";
-        // $check_stmt = $conn->prepare($check_sql);
-        // $check_stmt->execute([$student_name, $course_id]);
-        // $enrollment = $check_stmt->fetch();
-
-        // if ($enrollment) {
-            // Delete the course for the student
         $sql = "DELETE FROM `student_courses` WHERE student_name=? AND course_id=?";
         $stmt = $conn->prepare($sql);
         $result = $stmt->execute([$student_name, $course_id]);
@@ -36,16 +27,8 @@ function StudentCourseDelete(){
             $response['status'] = 400; 
             $response['message'] = "SQL錯誤";
         }
-        // } else {
-        //     $response['status'] = 404;
-        //     $response['message'] = "學生未註冊此課程";
-        // }
     }
 
     return($response); 
 }
-
-// Call the function and echo the response as JSON
-// $response = StudentCourseDelete();
-// echo json_encode($response);
 ?>
